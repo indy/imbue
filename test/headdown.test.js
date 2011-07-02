adf59f00483a3d5cc3d1dda999aabeb240c5b929
Inderjit Gill <email@indy.io>
2011-07-02 16:05:02 +0100

    finished initial prototype

--- test/headdown.test.js
+++ test/headdown.test.js
@@ -5,7 +5,6 @@ var fs = require('fs');
 var prefix = 'test/files/';
 var postfix = '.expected';
 
-
 // rather than this simple file comparison it would be better to 
 // parse the resultant html and do a DOM compare
 function fileCompare(fn, data, filename) {
@@ -36,15 +35,13 @@ exports['deep merge'] = function() {
              {a: "a string", b: {d: 3, e: 4}, c: [1, 2, 3]});
   assert.eql(headdown._deepMerge(beta, delta),
              {b: {d: 3, e: 4, f: 5, g: 6}});
-
 }
 
-
 exports['render without header'] = function() {
-  var locals = {locals: { names: ['foo', 'bar', 'baz']}};
+  var locals = { names: ['foo', 'bar', 'baz']};
   renderCompare(locals, 'no-header.hd');
 };
-/*
+
 exports['render with header'] = function() {
   renderCompare({}, 'header.hd');
 };
@@ -55,11 +52,9 @@ exports['render zonal'] = function() {
 };
 
 exports['render zonal with header'] = function() {
-  var zonal = {zonal: { names: ['foo', 'bar', 'baz']}};
+  var zonal = {zonal: { names: ['baq', 'baqq', 'baqqq']}};
   renderCompare(zonal, 'zonal-header.hd');
 };
-*/
 
-
-//var locals = {locals: { names: ['foo', 'bar', 'baz']}};
-//dbgOutput(headdown.fullRender, locals, 'test-render.hd');
+//var zonal = {zonal: { names: ['baq', 'baqq', 'baqqq']}};
+//dbgOutput(headdown.renderString, zonal, 'zonal-header.hd');
