becc21486515a0b0fd7217771c6d68d19cf67c76
Inderjit Gill <email@indy.io>
2011-07-02 13:19:04 +0100

    implemented deepMerge

--- test/headdown.test.js
+++ test/headdown.test.js
@@ -25,7 +25,20 @@ function renderCompare(data, filename) {
 
 exports['test version'] = function() {
   assert.equal(headdown.version, '0.0.1');
-};
+}
+
+exports['deep merge'] = function() {
+  var alpha = {a: "a string", c: [1, 2, 3]};
+  var beta = {b: {d: 3, e: 4}};
+  var delta = {b: {f: 5, g: 6}};
+
+  assert.eql(headdown._deepMerge(alpha, beta),
+             {a: "a string", b: {d: 3, e: 4}, c: [1, 2, 3]});
+  assert.eql(headdown._deepMerge(beta, delta),
+             {b: {d: 3, e: 4, f: 5, g: 6}});
+
+}
+
 
 exports['render without header'] = function() {
   var locals = {locals: { names: ['foo', 'bar', 'baz']}};
