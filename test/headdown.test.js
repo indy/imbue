2f255de2bced34b097c76bd27854f83b53c9a8dd
Inderjit Gill <email@indy.io>
2011-07-03 18:19:42 +0100

    added public compile function

--- test/headdown.test.js
+++ test/headdown.test.js
@@ -7,19 +7,22 @@ var postfix = '.expected';
 
 // rather than this simple file comparison it would be better to 
 // parse the resultant html and do a DOM compare
-function fileCompare(fn, data, filename) {
-  var input = fs.readFileSync(prefix + filename, 'utf8');
-  var expected = fs.readFileSync(prefix + filename + postfix, 'utf8');
-  assert.equal(fn(input, data), expected);
-}
 
 function dbgOutput(fn, data, filename) {
   var input = fs.readFileSync(prefix + filename, 'utf8');
   console.log(fn(input, data));
 }
 
-function renderCompare(data, filename) {
-  fileCompare(headdown.renderString, data, filename);
+function compare(data, filename) {
+  var input = fs.readFileSync(prefix + filename, 'utf8');
+  var expected = fs.readFileSync(prefix + filename + postfix, 'utf8');
+  assert.equal(headdown.renderString(input, data), expected);
+
+  var compiledFn = headdown.compile(input, {});
+  assert.equal(compiledFn(data), expected);
+
+  var compiledFn2 = headdown.compile(input, {bindings: data});
+  assert.equal(compiledFn2({}), expected);
 }
 
 exports['test version'] = function() {
@@ -28,21 +31,21 @@ exports['test version'] = function() {
 
 exports['render without header'] = function() {
   var locals = { names: ['foo', 'bar', 'baz']};
-  renderCompare(locals, 'no-header.hd');
+  compare(locals, 'no-header.hd');
 };
 
 exports['render with header'] = function() {
-  renderCompare({}, 'header.hd');
+  compare({}, 'header.hd');
 };
 
 exports['render zonal'] = function() {
   var zonal = {zonal: { names: ['foo', 'bar', 'baz']}};
-  renderCompare(zonal, 'zonal.hd');
+  compare(zonal, 'zonal.hd');
 };
 
 exports['render zonal with header'] = function() {
   var zonal = {zonal: { names: ['baq', 'baqq', 'baqqq']}};
-  renderCompare(zonal, 'zonal-header.hd');
+  compare(zonal, 'zonal-header.hd');
 };
 
 //var zonal = {zonal: { names: ['baq', 'baqq', 'baqqq']}};
