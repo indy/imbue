97b094720d627e63c348c605b617e9faf8c1297d
Inderjit Gill <email@indy.io>
2011-07-24 17:27:07 +0100

    added documentation to index.js

--- index.js
+++ index.js
@@ -4,4 +4,10 @@
  * MIT Licensed
  */
 
+
+// imbue is a markup library which allows meta-data to be specified in header sections. It's consists of the following files:
+//
+// * [imbue.js](imbue.html)
+// * [filters.js](filters.html)
+// * [utils.js](utils.html)
 module.exports = require('./lib/imbue');
